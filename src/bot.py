cb7ca4414fd03ad918cc32fb0139899a9915b55a
shuja-u <64989755+shuja-u@users.noreply.github.com>
2021-01-31 19:48:08 -0600

    Implemented boost chasing
    Bot now chases all boost pads that are active. Uses constant boosting to empty the boost meter, and uses constant handbraking to not get stuck in circles.

--- src/bot.py
+++ src/bot.py
@@ -41,20 +41,21 @@ class MyBot(BaseAgent):
         car_location = Vec3(my_car.physics.location)
         car_velocity = Vec3(my_car.physics.velocity)
         ball_location = Vec3(packet.game_ball.physics.location)
+        nearest_boost_loc = self.get_nearest_boost(car_location)
 
         # By default we will chase the ball, but target_location can be changed later
-        target_location = ball_location
+        target_location = nearest_boost_loc
 
-        if car_location.dist(ball_location) > 1500:
-            # We're far away from the ball, let's try to lead it a little bit
-            ball_prediction = self.get_ball_prediction_struct()  # This can predict bounces, etc
-            ball_in_future = find_slice_at_time(ball_prediction, packet.game_info.seconds_elapsed + 2)
-
-            # ball_in_future might be None if we don't have an adequate ball prediction right now, like during
-            # replays, so check it to avoid errors.
-            if ball_in_future is not None:
-                target_location = Vec3(ball_in_future.physics.location)
-                self.renderer.draw_line_3d(ball_location, target_location, self.renderer.cyan())
+        # if car_location.dist(ball_location) > 1500:
+        #     # We're far away from the ball, let's try to lead it a little bit
+        #     ball_prediction = self.get_ball_prediction_struct()  # This can predict bounces, etc
+        #     ball_in_future = find_slice_at_time(ball_prediction, packet.game_info.seconds_elapsed + 2)
+        #
+        #     # ball_in_future might be None if we don't have an adequate ball prediction right now, like during
+        #     # replays, so check it to avoid errors.
+        #     if ball_in_future is not None:
+        #         target_location = Vec3(ball_in_future.physics.location)
+        #         self.renderer.draw_line_3d(ball_location, target_location, self.renderer.cyan())
 
         # Draw some things to help understand what the bot is thinking
         self.renderer.draw_line_3d(car_location, target_location, self.renderer.white())
@@ -68,6 +69,8 @@ class MyBot(BaseAgent):
         controls = SimpleControllerState()
         controls.steer = steer_toward_target(my_car, target_location)
         controls.throttle = 1.0
+        controls.boost = True
+        controls.handbrake = True
         # You can set more controls if you want, like controls.boost.
 
         return controls
@@ -87,3 +90,15 @@ class MyBot(BaseAgent):
 
         # Return the controls associated with the beginning of the sequence so we can start right away.
         return self.active_sequence.tick(packet)
+
+    def get_nearest_boost(self, car_location: Vec3):
+        nearest_boost: Vec3 = None
+
+        for boost in self.boost_pad_tracker.boost_pads:
+            if not nearest_boost:
+                nearest_boost = Vec3(boost.location)
+            else:
+                if car_location.dist(Vec3(boost.location)) < car_location.dist(nearest_boost):
+                    nearest_boost = Vec3(boost.location)
+
+        return nearest_boost
